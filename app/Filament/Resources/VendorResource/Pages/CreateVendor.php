9b50ea2ac60a7fdff49a317c667b3d139d7b63b7
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-07-21 22:39:55 +0700

    💄 [VendorResource] style(formatting): standardize strict types declaration
    - use `declare(strict_types = 1);` instead of `declare(strict_types=1);`

--- app/Filament/Resources/VendorResource/Pages/CreateVendor.php
+++ app/Filament/Resources/VendorResource/Pages/CreateVendor.php
@@ -1,11 +1,10 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Filament\Resources\VendorResource\Pages;
 
 use App\Filament\Resources\VendorResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateVendor extends CreateRecord
