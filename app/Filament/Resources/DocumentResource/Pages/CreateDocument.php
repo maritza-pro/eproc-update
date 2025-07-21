3392742484efa9a64fe8666b3ae770eb14511c3d
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-07-21 22:39:07 +0700

    💄 [DocumentResource] style(formatting): apply code style changes
    - fix: apply code style to CreateDocument
    - fix: apply code style to EditDocument
    - fix: apply code style to ListDocuments
    - fix: apply code style to ViewDocument

--- app/Filament/Resources/DocumentResource/Pages/CreateDocument.php
+++ app/Filament/Resources/DocumentResource/Pages/CreateDocument.php
@@ -1,11 +1,10 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Filament\Resources\DocumentResource\Pages;
 
 use App\Filament\Resources\DocumentResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDocument extends CreateRecord
