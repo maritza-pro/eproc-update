847f480bd4813a7c5b6661db0a52a13df9f789ed
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-07-24 14:21:46 +0700

    ✨ [Surveys] feat(SurveyCategory): create SurveyCategoryResource
    - implements CRUD operations for managing survey categories
    - includes form, table, pages, and model definitions

---
+++ app/Filament/Resources/SurveyCategoryResource.php
@@ -0,0 +1,152 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Filament\Resources;
+
+use App\Concerns\Resource\Gate;
+use App\Filament\Resources\SurveyCategoryResource\Pages;
+use App\Models\SurveyCategory;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Hexters\HexaLite\HasHexaLite;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
+use Rmsramos\Activitylog\Actions\ActivityLogTimelineTableAction;
+use Rmsramos\Activitylog\RelationManagers\ActivitylogRelationManager;
+
+class SurveyCategoryResource extends Resource
+{
+    use Gate {
+        Gate::defineGates insteadof HasHexaLite;
+    }
+    use HasHexaLite;
+
+    protected static ?string $model = SurveyCategory::class;
+
+    protected static ?string $modelLabel = 'Survey Category';
+
+    protected static ?string $navigationGroup = 'Surveys';
+
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required(),
+                        Forms\Components\Textarea::make('description'),
+                        Forms\Components\Grid::make(3)
+                            ->schema([
+                                Forms\Components\TextInput::make('type'),
+                                Forms\Components\TextInput::make('code'),
+                                Forms\Components\Select::make('parent_id')
+                                    ->relationship('parent', 'name'),
+                            ]),
+                        Forms\Components\Grid::make(3)
+                            ->schema([
+                                Forms\Components\ColorPicker::make('text_color'),
+                                Forms\Components\ColorPicker::make('background_color'),
+                                Forms\Components\Toggle::make('is_active')
+                                    ->inline(false)
+                                    ->required()
+                                    ->default(true),
+                            ]),
+                    ]),
+            ]);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSurveyCategories::route('/'),
+            'create' => Pages\CreateSurveyCategory::route('/create'),
+            'view' => Pages\ViewSurveyCategory::route('/{record}'),
+            'edit' => Pages\EditSurveyCategory::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            ActivitylogRelationManager::class,
+        ];
+    }
+
+    public static function table(Table $table): Table
+    {
+        $withoutGlobalScope = ! Auth::user()?->can(static::getModelLabel() . '.withoutGlobalScope');
+
+        return $table
+            ->deferLoading()
+            ->striped()
+            ->columns([
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('parent.name')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\ColorColumn::make('text_color')
+                    ->alignCenter(),
+                Tables\Columns\ColorColumn::make('background_color')
+                    ->alignCenter(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean()
+                    ->alignCenter(),
+                Tables\Columns\IconColumn::make('is_system')
+                    ->boolean()
+                    ->alignCenter()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                ActivityLogTimelineTableAction::make('Activities'),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
