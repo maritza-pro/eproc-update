17e33453b74b4c313994089f2c377961e6c2f5d9
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-07-24 08:52:15 +0700

    ✨ [Base] feat(vscode): add vscode settings for development
    - add recommended extensions for vscode in extensions.json
    - add settings for vscode in settings.json
    - remove .vscode from .gitignore
    - comment order print route in web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect()->route('filament.dashboard.pages.dashboard'));
 
-Route::get('/dashboard/orders/{order}/print', [\App\Actions\PrintOrder::class, 'handle'])
-    ->name('order.print')
-    ->middleware(['auth', 'verified'])
-    ->where('order', '[0-9]+');
+// Route::get('/dashboard/orders/{order}/print', [\App\Actions\PrintOrder::class, 'handle'])
+//     ->name('order.print')
+//     ->middleware(['auth', 'verified'])
+//     ->where('order', '[0-9]+');
