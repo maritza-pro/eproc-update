887a24586b2f049255e0eb338c91b0193da8fc08
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-07-24 14:53:05 +0700

    ✨ [Survey] feat(question): create SurveyQuestion model and migration
    - add SurveyQuestion model with relationships and types
    - create migration for survey_questions table

---
+++ app/Models/SurveyQuestion.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class SurveyQuestion extends Model
+{
+    use LogsActivity,
+        SoftDeletes;
+
+    const array TYPES = [
+        self::TYPE_TEXT,
+        self::TYPE_RADIO,
+        self::TYPE_CHECKBOX,
+        self::TYPE_SELECT,
+        self::TYPE_DATE,
+        self::TYPE_FILE,
+    ];
+
+    const string TYPE_CHECKBOX = 'checkbox';
+
+    const string TYPE_DATE = 'date';
+
+    const string TYPE_FILE = 'file';
+
+    const string TYPE_RADIO = 'radio';
+
+    const string TYPE_SELECT = 'select';
+
+    const string TYPE_TEXT = 'text';
+
+    protected $fillable = [
+        'survey_id',
+        'question',
+        'type',
+        'description',
+        'required',
+        'points',
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
+
+    public function survey(): BelongsTo
+    {
+        return $this->belongsTo(Survey::class);
+    }
+}
