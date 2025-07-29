fe2c75011d9ee1496286dbc65824187e8d347818
Ferdy Mohamad Firdaus <kzuferdy@gmail.com>
2025-07-29 10:19:57 +0700

    ✨ [Base] feat(BusinessField): add relationship with Vendor model
    - add vendors relationship to BusinessField model
    - use HasFactory trait

--- app/Models/BusinessField.php
+++ app/Models/BusinessField.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 
 class BusinessField extends Model
 {
     use LogsActivity;
     use SoftDeletes;
+	use HasFactory;
 
    protected $fillable = [
         'code',
@@ -23,5 +27,10 @@ class BusinessField extends Model
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults();
+    }
+
+	public function vendors(): HasMany
+    {
+        return $this->hasMany(Vendor::class);
     }
 }
