b43b1d4db0147788459c42b9dd5455d001a7b73e
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-07-30 14:02:50 +0700

    ✨ [Base] feat(database): create currencies table
    - Creates 'currencies' table migration
    - Adds columns: name, code, symbol, decimals, symbol_position, is_default
    - Adds soft delete functionality

--- database/migrations/2025_07_29_065318_create_currencies_table.php
+++ database/migrations/2025_07_29_065318_create_currencies_table.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -12,23 +12,30 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-{
-    Schema::create('currencies', function (Blueprint $table) {
-        $table->id();
-        $table->string('name')
-			->comment('The name of the currency, e.g., Indonesian Rupiah');
-        $table->string('code', 3)->unique()
-			->comment('The code of the currency, e.g., IDR');
-        $table->string('symbol', 5)->unique()
-			->comment('The symbol of the currency, e.g., Rp');
-        $table->unsignedTinyInteger('decimals')->default(2)->comment('Number of decimal places');
-        $table->string('symbol_position', 10)->default('left')->comment('Posisition symbol: left or right');
-        $table->boolean('is_default')->default(false)
-			->comment('Whether the currency is the default currency or not');
-        $table->timestamps();
-        $table->softDeletes();
-    });
-}
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')
+                ->comment('The name of the currency, e.g., Indonesian Rupiah');
+            $table->string('code', 3)
+                ->comment('The code of the currency, e.g., IDR');
+            $table->string('symbol', 5)
+                ->comment('The symbol of the currency, e.g., Rp');
+            $table->unsignedTinyInteger('decimals')
+                ->nullable()
+                ->default(2)
+                ->comment('Number of decimal places');
+            $table->string('symbol_position', 10)
+                ->nullable()
+                ->default('left')
+                ->comment('Posisition symbol: left or right');
+            $table->boolean('is_default')
+                ->nullable()
+                ->default(false)
+                ->comment('Whether the currency is the default currency or not');
+            $table->timestampSoftDelete();
+        });
+    }
 
     /**
      * Reverse the migrations.
