88ac8bec255eeef118e5727a424d9dba0ac28ffd
Ferdy Mohamad Firdaus <kzuferdy@gmail.com>
2025-07-30 10:57:09 +0700

    ✨ [CurrencyResource] feat(Pages): create view currency page
    - add view currency page with back and edit actions

---
+++ app/Filament/Resources/CurrencyResource/Pages/ViewCurrency.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\CurrencyResource\Pages;
+
+use App\Filament\Resources\CurrencyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewCurrency extends ViewRecord
+{
+    protected static string $resource = CurrencyResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('back')
+                ->label('Back')
+                ->icon('heroicon-m-arrow-left')
+                ->color('gray')
+                ->url(static::getResource()::getUrl('index')),
+            Actions\EditAction::make(),
+        ];
+    }
+}
