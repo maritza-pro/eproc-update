1256cf384aad5de374d5fc6491a0f2c8114072cb
Ferdy Mohamad Firdaus <kzuferdy@gmail.com>
2025-07-30 10:57:36 +0700

    ✨ [Currency] feat(Resource): create currency creation page

---
+++ app/Filament/Resources/CurrencyResource/Pages/CreateCurrency.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\CurrencyResource\Pages;
+
+use App\Filament\Resources\CurrencyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCurrency extends CreateRecord
+{
+    protected static string $resource = CurrencyResource::class;
+}
