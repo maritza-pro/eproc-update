5066a8fc613257c88d9e83210f8b85f12886abb2
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-07-30 14:13:30 +0700

    ♻️ [CurrencyResource] refactor(Pages): standardize strict type declarations
    - use strict_types = 1 instead of strict_types=1

--- app/Filament/Resources/CurrencyResource/Pages/CreateCurrency.php
+++ app/Filament/Resources/CurrencyResource/Pages/CreateCurrency.php
@@ -1,11 +1,10 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Filament\Resources\CurrencyResource\Pages;
 
 use App\Filament\Resources\CurrencyResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCurrency extends CreateRecord
