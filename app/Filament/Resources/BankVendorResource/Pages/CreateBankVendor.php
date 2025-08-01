1ee5d0ef35a18dfd815cc430e3654e7a58c16b01
Ferdy Mohamad Firdaus <kzuferdy@gmail.com>
2025-08-01 07:25:52 +0700

    ✨ [BankVendor] feat(Pages): create bank vendor page
    - create new CreateBankVendor page under BankVendorResource
    diff --git a/app/Filament/Resources/BankVendorResource/Pages/EditBankVendor.php b/app/Filament/Resources/BankVendorResource/Pages/EditBankVendor.php
    new file mode 100644
    index 0000000..7c28961
    --- /dev/null
    +++ b/app/Filament/Resources/BankVendorResource/Pages/EditBankVendor.php
    @@ -0,0 +1,15 @@
    +<?php
    +
    +declare(strict_types=1);
    +
    +namespace App\Filament\Resources\BankVendorResource\Pages;
    +
    +use App\Filament\Resources\BankVendorResource;
    +use Filament\Actions;
    +use Filament\Resources\Pages\EditRecord;
    +
    +class EditBankVendor extends EditRecord
    +{
    +    protected static string $resource = BankVendorResource::class;
    +}
    +

---
+++ app/Filament/Resources/BankVendorResource/Pages/CreateBankVendor.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\BankVendorResource\Pages;
+
+use App\Filament\Resources\BankVendorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBankVendor extends CreateRecord
+{
+    protected static string $resource = BankVendorResource::class;
+}
