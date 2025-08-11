3a4fcf1b9a40aedcf6126c8fa8ad229633aa3ece
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-08-11 12:26:15 +0700

    🔧 [Base] chore(database): support database socket connection
    - add support for database socket connection
    - set host and port to null if DB_SOCKET is defined

--- config/database.php
+++ config/database.php
@@ -47,8 +47,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_SOCKET') ? null : env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_SOCKET') ? null : env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
