e4d33ea07606d8ec918f6989858379676e037748
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-08-15 15:12:09 +0700

    📝 [BidItemResource] docs(Pages): add documentation to header actions
    - add documentation to getHeaderActions function in EditBidItem, ListBidItems and ViewBidItem

--- app/Filament/Resources/BidItemResource/Pages/EditBidItem.php
+++ app/Filament/Resources/BidItemResource/Pages/EditBidItem.php
@@ -12,6 +12,11 @@ class EditBidItem extends EditRecord
 {
     protected static string $resource = BidItemResource::class;
 
+    /**
+     * Get the header actions.
+     *
+     * Defines the actions available in the record header.
+     */
     protected function getHeaderActions(): array
     {
         return [
