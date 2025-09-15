13f4543d0935737ca395346b668b45641794fbe4
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2025-09-15 15:21:18 +0700

    🐛 [MasterProcurements] fix(Schedule): remove schedule todo comment
    - Remove schedule todo comment to cleanup code

--- app/Filament/Clusters/MasterProcurements/Resources/ScheduleResource/Pages/ManageSchedules.php
+++ app/Filament/Clusters/MasterProcurements/Resources/ScheduleResource/Pages/ManageSchedules.php
@@ -8,7 +8,6 @@ use App\Filament\Clusters\MasterProcurements\Resources\ScheduleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
-// TODO : @rizkyxp Agenda / Agendas coba diskus sama maup yee
 class ManageSchedules extends ManageRecords
 {
     protected static string $resource = ScheduleResource::class;
